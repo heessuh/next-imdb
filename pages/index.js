5c0624279e61ecea981e0f6f423bcc68355a0e87
Fred Suh <50889806+heessuh@users.noreply.github.com>
2022-10-17 19:46:55 -0300

    added components

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
 import Head from "next/head"
-import Image from "next/image"
-import styles from "../styles/Home.module.css"
+import Header from "../components/Header.js"
+import Navbar from "../components/Navbar.js"
+import Results from "../components/Results.js"
 
 export default function Home() {
-  return <div className="text-sm font-serif text-blue-600">Hi world!</div>
+  return (
+    <div className="text-sm font-serif text-blue-600">
+      <Head>
+        <meta name="description" content="Generate head" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+      <Navbar />
+      <Results />
+    </div>
+  )
 }
